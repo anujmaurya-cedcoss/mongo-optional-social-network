ef24495a03242076cf5505e729197e4809dcf864
Anuj Maurya <anujmaurya@cedcoss.com>
2023-06-06 09:48:41 +0530

    completed

--- src/app/controllers/ChatController.php
+++ src/app/controllers/ChatController.php
@@ -2,6 +2,7 @@
 
 use Phalcon\Mvc\Controller;
 
+session_start();
 class ChatController extends Controller
 {
     public function indexAction()
@@ -12,10 +13,18 @@ class ChatController extends Controller
 
     public function addAction()
     {
-        $id = 1;
+        $id = $_SESSION['id'];
         $arr[$id]['message'] = $_POST['message'];
         $arr = json_encode($arr);
-        $this->mongo->messages->updateOne(['_id' => new \MongoDB\BSON\ObjectID('647dd066cdd1253ee009c33e')], ['$push' => ['messages' => json_decode($arr)]]);
+        $this->mongo->messages->updateOne(
+            ['_id' => new \MongoDB\BSON\ObjectID('647dd066cdd1253ee009c33e')],
+            ['$push' => ['messages' => json_decode($arr)]]
+        );
         $this->response->redirect('/chat/');
+    }
+    public function logoutAction()
+    {
+        session_unset();
+        session_destroy();
     }
 }
--- src/public/index.php
+++ src/public/index.php
@@ -6,6 +6,7 @@ use Phalcon\Mvc\View;
 use Phalcon\Mvc\Application;
 use Phalcon\Url;
 use Phalcon\Config;
+use handler\Listener\Listener;
 
 $config = new Config([]);
 
@@ -23,6 +24,10 @@ $loader->registerDirs(
         APP_PATH . "/controllers/",
     ]
 );
+
+$loader->registerNamespaces([
+    "handler\Listener" => APP_PATH . "/handlers/",
+]);
 
 $loader->register();
 
@@ -59,6 +64,15 @@ $container->set(
 );
 
 $application = new Application($container);
+$eventsManager = $container->get('eventsManager');
+
+$eventsManager->attach(
+    'application:beforeHandleRequest',
+    new Listener()
+);
+$container->set('EventsManager', $eventsManager);
+$application->setEventsManager($eventsManager);
+
 
 // injecting response
 $container->set(
